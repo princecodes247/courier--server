e7321c52a0b8f277ef99f17785e48ee39ba2c121
Prince Codes <princecodes247@gmail.com>
2022-05-25 14:51:12 -0400

    init

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,40 @@
+const AuthService = require("../services/auth.service");
+const response = require("../utils/response");
+
+class AuthContoller {
+  async signup(req, res) {
+    const result = await AuthService.signup(req.body);
+    res.status(201).send(response("User created", result));
+  }
+
+  async signin(req, res) {
+    const result = await AuthService.signin(req.body);
+    res.status(200).send(response("User login successful", result));
+  }
+
+  async updatePassword(req, res) {
+    const result = await AuthService.updatePassword(req.params.userId, req.body);
+    res.status(200).send(response("Password updated", result));
+  }
+
+  async RequestEmailVerification(req, res) {
+    const result = await AuthService.RequestEmailVerification(req.query.email);
+    res.status(200).send(response("Email verfication link sent", result));
+  }
+
+  async VerifyEmail(req, res) {
+    const result = await AuthService.VerifyEmail(req.body);
+    res.status(200).send(response("Email verified successfully", result));
+  }
+
+  async RequestPasswordReset(req, res) {
+    const result = await AuthService.RequestPasswordReset(req.query.email);
+    res.status(200).send(response("Password reset link sent", result));
+  }
+  async resetPassword(req, res) {
+    const result = await AuthService.resetPassword(req.body);
+    res.status(200).send(response("Password updated", result));
+  }
+}
+
+module.exports = new AuthContoller();
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,31 @@
+const UserService = require("./../services/user.service");
+const response = require("./../utils/response");
+
+class UserContoller {
+  async create(req, res) {
+    const result = await UserService.create(req.body);
+    res.status(200).send(response("User created", result));
+  }
+
+  async getAll(req, res) {
+    const result = await UserService.getAll();
+    res.status(200).send(response("All users", result));
+  }
+
+  async getOne(req, res) {
+    const result = await UserService.getOne(req.params.userId);
+    res.status(200).send(response("User data", result));
+  }
+
+  async update(req, res) {
+    const result = await UserService.update(req.params.userId, req.body);
+    res.status(200).send(response("User updated", result));
+  }
+
+  async delete(req, res) {
+    const result = await UserService.delete(req.params.userId);
+    res.status(200).send(response("User deleted", result));
+  }
+}
+
+module.exports = new UserContoller();
