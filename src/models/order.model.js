db8b8f1672169d941b12c1cb9e20bf3a4bd6921f
Prince Codes <princecodes247@gmail.com>
2022-05-25 18:20:23 -0400

    feat: update order model

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,9 +1,44 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const OrderSchema = new Schema({
-     name: String
-});
+const OrderSchema = new Schema(
+     {
+          name: {
+            type: String,
+            trim: true,
+            required: [true, "Name is required"],
+          },
+          email: {
+            type: String,
+            trim: true,
+            unique: true,
+            required: [true, "Email is required"],
+          },
+          password: {
+            type: String,
+          },
+          image: {
+            type: String,
+          },
+          role: {
+            type: String,
+            trim: true,
+            enum: ["user", "admin"],
+            default: "user"
+          },
+          isActive: {
+            type: Boolean,
+            default: true,
+          },
+          isVerified: {
+            type: Boolean,
+            default: false,
+          }
+        },
+        {
+          timestamps: true
+        }
+);
 
 
 module.exports = mongoose.model("Order", OrderSchema)
