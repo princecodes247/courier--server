a0a7cd717795b15063f9df0925bdcac73e3670fe
Prince Codes <princecodes247@gmail.com>
2022-05-27 06:14:50 -0400

    test

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
 const OrderController = require("../controllers/order.controller");
-const auth = require('../middlewares/auth.middleware');
+const auth = require("../middlewares/auth.middleware");
 const { role } = require("../config");
 
-router.post("/", auth(role.ADMIN), OrderController.create);
-router.get("/", auth(role.ADMIN), OrderController.getAll);
-router.get("/:orderId" ,OrderController.getOne);
-router.put("/:orderId", auth(role.ADMIN), OrderController.update);
-router.delete("/:orderId", auth(role.ADMIN), OrderController.delete);
+router.post("/", OrderController.create);
+router.get("/", OrderController.getAll);
+// router.post("/", auth(role.ADMIN), OrderController.create);
+// router.get("/", auth(role.ADMIN), OrderController.getAll);
+router.get("/:orderId", OrderController.getOne);
+router.put("/:orderId", OrderController.update);
+router.delete("/:orderId", OrderController.delete);
+// router.put("/:orderId", auth(role.ADMIN), OrderController.update);
+// router.delete("/:orderId", auth(role.ADMIN), OrderController.delete);
 
-
-module.exports = router
+module.exports = router;
