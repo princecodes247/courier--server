aace603d81b3927e78112c8de3c9777a26998890
Prince Codes <princecodes247@gmail.com>
2022-05-27 06:14:38 -0400

    update order model

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,34 +1,39 @@
 const mongoose = require("mongoose");
-const shortid = require('shortid');
+const shortid = require("shortid");
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema(
-     {    
-          _id: {
-               type: String,
-               default: shortid.generate
-          },
-          name: {
-            type: String,
-          },
-          products: {
-            type: Array,
-            required: [true, "Products are required"],
-          },
-          price: {
-            type: Number,
-               required: [true, "Price is required"],
-          },
-          status: {
-            type: String,
-            trim: true,
-            enum: ["processing", "shipped", "delivered", "cancelled"],
-               default: "pending",
-          }
-        },
-        {
-          timestamps: true
-        }
+  {
+    _id: {
+      type: String,
+      default: shortid.generate,
+    },
+    from: {
+      type: String,
+      required: [true, "Start location is required"],
+    },
+    to: {
+      type: String,
+      required: [true, "End location is required"],
+    },
+    // products: {
+    //   type: Array,
+    //   required: [true, "Products are required"],
+    // },
+    // price: {
+    //   type: Number,
+    //      required: [true, "Price is required"],
+    // },
+    status: {
+      type: String,
+      trim: true,
+      enum: ["pending", "processing", "in transit", "delivered", "cancelled"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model("Order", OrderSchema)
+module.exports = mongoose.model("Order", OrderSchema);
